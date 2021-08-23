c4453bbdf18ce3e7f68882f748ba734b54674dd4
gokul1630 <gokulgp163034@gmail.com>
2021-08-23 09:49:35 +0530

    use .env

--- src/utils/client.js
+++ src/utils/client.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-import { BACKEND_URL } from '../constants'
+const BACKEND_URL = process.env.REACT_APP_BACKEND_URL
 
 const client = async (endPoint, { ...configs }) => {
   let data = localStorage.getItem('token')
